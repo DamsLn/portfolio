496082b785c8f979eeea5031832f7f5dae62c837
Damien <lasson.damienpro@gmail.com>
2022-09-19 19:10:55 +0200

    feat: sets up the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,45 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '390px',
+      'md': '820px',
+      'lg': '1100px',
+      'xl': '1600px',
+    },
     fontFamily: {
       'sfPro': ['SF-Pro']
     },
+    fontSize: {
+      '68': '68px',
+      '32': '32px',
+      '26': '26px',
+      '24': '24px',
+      '20.8': '20.8px',
+      '16': '16px',
+    },
     colors: {
       'primary': '#7A82AB',
+      'primary-dark': '#616786',
       'white': '#FFFFFF',
       'black': '#000000',
+      'grey': '#F2F3F4',
       'grey-light': '#979A9A',
       'grey-dark': '#AFAFAF',
       'dark-blue': '#34495E',
       'green': '#52BE80',
       'yellow': '#F7DC6F',
+    },
+    spacing: {
+      '5': '5px',
+      '8': '8px',
+      '10': '10px',
+      '13': '13px',
+      '20': '20px',
+      '24': '24px',
+      '41': '41px',
+      '117': '117px',
+      '153': '153px',
     }
   },
   plugins: [],
